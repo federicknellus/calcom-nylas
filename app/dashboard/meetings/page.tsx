f2f3df42328c5b96da00985e79e747c55df76615
Federico Simonelli <98852671+federicknellus@users.noreply.github.com>
2025-01-06 19:58:29 +0100

    fixato errore sui partecipanti ai meeting

--- app/dashboard/meetings/page.tsx
+++ app/dashboard/meetings/page.tsx
@@ -101,7 +101,7 @@ const MeetingsPage = async () => {
                   <div className="flex flex-col items-start">
                     <h2 className="text-sm font-medium">{item.title}</h2>
                     <p className="text-sm text-muted-foreground">
-                      Tu e {item.participants[0].name || item.participants[0].email}
+                      Tu e {item.participants[0]?.name || item.participants[0]?.email || "basta"}
                     </p>
 
                   </div>
